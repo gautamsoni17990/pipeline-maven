0456d70bbdf1a8db7864553e499f9c7d71886c24
Gautam Soni <gautam.soni17990@gmail.com>
2021-08-31 21:06:56 +0000

    POST action has been added

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,7 +4,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/vagrant/prod /tmp/.auth ubuntu@server.local:/tmp/.auth
-scp -i /home/vagrant/prod ./jenkins/deploy/publish.sh ubuntu@server.local:/tmp/publish
-ssh -i /home/vagrant/prod ubuntu@server.local "/tmp/publish"
+scp -i /opt/prod /tmp/.auth ubuntu@server.local:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh ubuntu@server.local:/tmp/publish
+ssh -i /opt/prod ubuntu@server.local "/tmp/publish"
 
